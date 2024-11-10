61635cc3fd01c316d86dbe91ef12d78d3188a243
Noza23 <G.Nozadze@campus.lmu.de>
2024-11-10 23:51:51 +0100

    tests: add tests for attribute docstrings

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -6,15 +6,31 @@ from dataserious.fields import ConfigField
 class Coords(BaseConfig):
     x: int = ConfigField(searchable=True, description="xy")
     y: int
-    z: str = ConfigField(False, description="xy")
+    """This is an attribute docstring for y."""
+    z: str = ConfigField(False, description="yz")
+    """This wont override the description in ConfigField."""
+    ref: "Coords"
+    """This is an multiline
+    attribute docstring for ref.
+    """
 
 
 def test_config_field():
-    assert Coords.__dataclass_fields__["x"].metadata["searchable"] is True
-    assert Coords.__dataclass_fields__["x"].metadata["description"] == "xy"
+    assert Coords.__dataclass_fields__["x"].metadata.get("searchable") is True
+    assert Coords.__dataclass_fields__["x"].metadata.get("description") == "xy"
     assert Coords.__dataclass_fields__["y"].metadata.get("searchable") is None
-    assert Coords.__dataclass_fields__["y"].metadata.get("description") is None
     assert Coords.__dataclass_fields__["y"].default is MISSING
-    assert Coords.__dataclass_fields__["z"].metadata["searchable"] is False
-    assert Coords.__dataclass_fields__["z"].metadata["description"] == "xy"
-    # Searchable Field with Description
+    assert Coords.__dataclass_fields__["z"].metadata.get("searchable") is False
+    assert Coords.__dataclass_fields__["z"].metadata.get("description") == "yz"
+    assert Coords.__dataclass_fields__["ref"].metadata.get("searchable") is None
+
+
+def test_attr_descriptions():
+    Coords.__dataclass_fields__["x"].metadata["description"] == "xy"
+    Coords.__dataclass_fields__["y"].metadata[
+        "description"
+    ] == "This is an attribute docstring for y."
+    Coords.__dataclass_fields__["z"].metadata["description"] == "yz"
+    Coords.__dataclass_fields__["ref"].metadata[
+        "description"
+    ] == "This is an multiline attribute docstring for ref."
