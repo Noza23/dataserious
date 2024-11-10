bf31424f77d24ddfa2b1981bc5de306d2774f485
Noza23 <G.Nozadze@campus.lmu.de>
2024-11-10 23:50:42 +0100

    feat: attribute docstrings

--- dataserious/fields.py
+++ dataserious/fields.py
@@ -1,5 +1,7 @@
 """Fields for dataserious configs."""
 
+import ast
+import inspect
 from dataclasses import field
 from typing import Callable, ParamSpec, TypeVar
 
@@ -14,10 +16,10 @@ def _extend_signature(func: Callable[P, T]):
         *args: P.args,
         **kwargs: P.kwargs,
     ):
-        metadata = kwargs.setdefault("metadata", {})
-        if isinstance(metadata, dict):
-            metadata["searchable"] = searchable
-            metadata["description"] = description
+        kwargs.setdefault("metadata", {})
+        if isinstance(kwargs["metadata"], dict):
+            kwargs["metadata"]["searchable"] = searchable
+            kwargs["metadata"]["description"] = description
         return field(**kwargs)  # type: ignore
 
     return _ConfigField
@@ -26,3 +28,42 @@ def _extend_signature(func: Callable[P, T]):
 # ConfigField is a constructor for fields of a BaseConfig class extending
 # dataclasses.field with some additional arguments.
 ConfigField = _extend_signature(field)
+
+
+class DescriptionVisitor(ast.NodeVisitor):
+    """Extracts attribute docstrings as descriptions from a class definition."""
+
+    def __init__(self):  # noqa: D107
+        super().__init__()
+        self.descriptions: dict[str, str] = {}
+        self.target: str | None = None
+
+    def visit(self, node):  # noqa: D102
+        if isinstance(node, ast.AnnAssign) and isinstance(node.target, ast.Name):
+            self.target = node.target.id
+            self.descriptions[self.target] = ""
+        super().visit(node)
+
+    def visit_Expr(self, node):  # noqa: D102
+        if isinstance(node.value, ast.Constant) and isinstance(node.value.value, str):
+            desc = inspect.cleandoc(node.value.value)
+            if self.target is not None:
+                self.descriptions[self.target] = desc.replace("\n", " ")
+
+
+def get_attr_descriptions(cls: type[object]) -> dict[str, str]:
+    """Goes through the class definition and extracts attribute docstrings.
+
+    Args:
+        cls: The class to extract the attribute docstrings from.
+
+    Returns:
+        A dictionary with the attribute names as keys and the docstrings as values.
+    """
+    try:
+        source = inspect.getsource(cls)
+    except OSError:
+        return {}
+    visitor = DescriptionVisitor()
+    visitor.visit(ast.parse(source))
+    return visitor.descriptions
