ff72fee37d808dee34acf5a2d5d9b0d22fa769e0
Giorgi <104214511+Noza23@users.noreply.github.com>
2025-03-16 16:24:20 +0100

    feat: add readme (#14)

--- dataserious/__init__.py
+++ dataserious/__init__.py
@@ -1,5 +1,6 @@
 """Python dataclasses enhanced with type validation and serialization."""
 
 from .base import BaseConfig
+from .fields import ConfigField
 
-__all__ = ["BaseConfig"]
+__all__ = ["BaseConfig", "ConfigField"]
